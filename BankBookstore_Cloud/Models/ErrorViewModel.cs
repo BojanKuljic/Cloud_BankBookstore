7a5f9d2a7c0a5f32a20e450e416177bd7e2b3f50
Bojan Kuljić <116057046+BojanKuljic@users.noreply.github.com>
2024-10-30 19:08:13 +0100

    Add project files.

---
+++ BankBookstore_Cloud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankBookstore_Cloud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
