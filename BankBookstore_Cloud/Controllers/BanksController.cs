f2051e40c76a7a5c1be135a6bd5a7eea36ce7285
Bojan Kuljić <116057046+BojanKuljic@users.noreply.github.com>
2024-11-17 19:39:41 +0100

    Add implementation of Clients Controllers

---
+++ BankBookstore_Cloud/Controllers/BanksController.cs
@@ -0,0 +1,61 @@
+﻿using Client.Models;
+using Common.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Newtonsoft.Json;
+
+namespace Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BanksController : Controller
+    {
+
+
+        [HttpGet]
+        [Route("ListBanksClients")]
+        public async Task<IActionResult> ListBanksClients()
+        {
+            IValidation? validationProxy = ServiceProxy.Create<IValidation>(new Uri("fabric:/Cloud_BankBookstore/Validation"));
+
+            List<string> result = await validationProxy.ListBanksClients();
+
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home", new { errorMessage = "Cannot get clients." });
+            }
+
+            var clients = new List<BankClientModel>();
+
+            result.ForEach(x => clients.Add(JsonConvert.DeserializeObject<BankClientModel>(x)!));
+
+            return View(clients);
+        }
+
+
+        [HttpGet]
+        [Route("EnlistMoneyTransfer")]
+        public async Task<IActionResult> EnlistMoneyTransfer(long userSendId)
+        {
+            return View(new EnlistMoneyTransferModel() { UserSendId = userSendId });
+        }
+
+        [HttpPost]
+        [Route("EnlistMoneyTransfer")]
+        public async Task<IActionResult> EnlistMoneyTransfer([FromForm] EnlistMoneyTransferModel model)
+        {
+            IValidation? validationProxy = ServiceProxy.Create<IValidation>(new Uri("fabric:/Cloud_BankBookstore/Validation"));
+
+            string result = await validationProxy.EnlistMoneyTransfer(model.UserSendId, model.UserReceiveId, model.Amount);
+
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home", new { errorMessage = "Cannot execute money transfer." });
+            }
+
+            return RedirectToAction("ListBanksClients", "Banks");
+        }
+
+
+    }
+}
---
+++ BankBookstore_Cloud/Controllers/BookstoresController.cs
@@ -0,0 +1,97 @@
+﻿using Client.Models;
+using Common.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Newtonsoft.Json;
+
+namespace Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookstoresController : Controller
+    {
+        [HttpGet]
+        [Route("ListAvailableBooks")]
+        public async Task<IActionResult> ListAvailableBooks()
+        {
+            IValidation? validationProxy = ServiceProxy.Create<IValidation>(new Uri("fabric:/Cloud_BankBookstore/Validation"));
+
+            List<string> result = await validationProxy.ListAvailableBooks();
+
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home", new { errorMessage = "Cannot get list of books." });
+            }
+
+            var books = new List<BookModel>();
+
+            result.ForEach(x => books.Add(JsonConvert.DeserializeObject<BookModel>(x)!));
+
+            return View(books);
+        }
+
+
+        [HttpGet]
+        [Route("EnlistPurchase")]
+        public async Task<IActionResult> EnlistPurchase(long bookId, uint count)
+        {
+            return View(new EnlistPurchaseModel() { BookId = bookId, Count = count });
+        }
+
+        [HttpPost]
+        [Route("EnlistPurchase")]
+        public async Task<IActionResult> EnlistPurchase([FromForm] EnlistPurchaseModel model)
+        {
+            IValidation? validationProxy = ServiceProxy.Create<IValidation>(new Uri("fabric:/Cloud_BankBookstore/Validation"));
+
+            string result = await validationProxy.EnlistPurchase(model.BookId, model.Count);
+
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home", new { errorMessage = "Cannot purchase." });
+            }
+
+            return RedirectToAction("ListAvailableBooks", "Bookstores");
+        }
+
+        [HttpGet]
+        [Route("GetItemPrice/{id}")]
+        public async Task<IActionResult> GetBookPrice(long id)
+        {
+            var validationProxy = ServiceProxy.Create<IValidation>(new Uri("fabric:/Cloud_BankBookstore/Validation"));
+
+            var result = await validationProxy.GetBookPrice(id);
+
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home", new { errorMessage = "Cannot get book's price." });
+            }
+
+            double price = JsonConvert.DeserializeObject<double>(result);
+
+            return View(price);
+        }
+
+        [HttpGet]
+        [Route("GetBook/{id}")]
+        public async Task<IActionResult> GetBook(long id)
+        {
+            var validationProxy = ServiceProxy.Create<IValidation>(new Uri("fabric:/Cloud_BankBookstore/Validation"));
+
+            string result = await validationProxy.GetBook(id);
+
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home", new { errorMessage = "Cannot get book." });
+            }
+
+            BookModel book = JsonConvert.DeserializeObject<BookModel>(result)!;
+
+            return View(book);
+        }
+
+
+
+
+    }
+}
