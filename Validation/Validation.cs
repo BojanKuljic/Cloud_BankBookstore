fa86029e3fd51ef0df05a93d5458deb0bf5ec94c
Bojan Kuljić <116057046+BojanKuljic@users.noreply.github.com>
2024-11-17 19:01:44 +0100

    Add and realise IValidation interface

--- Validation/Validation.cs
+++ Validation/Validation.cs
@@ -1,47 +1,125 @@
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
-
 using Microsoft.ServiceFabric.Services.Runtime;
 using System.Fabric;
 using Common.Interfaces;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
 
 namespace Validation
 {
-     internal sealed class Validation : StatelessService//, IValidation
+    internal sealed class Validation : StatelessService, IValidation
     {
+
+        private readonly string transactionCoordinatorPath = @"fabric:/Cloud_BankBookstore/TransactionCoordinator";
+
         public Validation(StatelessServiceContext context)
             : base(context)
         { }
 
-        //public Task<string> EnlistMoneyTransfer(long userSend, long userReceive, double amount)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public async Task<List<string>> ListAvailableBooks()
+        {
+            ITransactionCoordinator? transactionProxy = ServiceProxy.Create<ITransactionCoordinator>(new Uri(transactionCoordinatorPath));
 
-        //public Task<string> EnlistPurchase(long bookId, uint count)
-        //{
-        //    throw new NotImplementedException();
-        //}
+            try
+            {
+                return await transactionProxy.ListAvailableBooks();
+            }
+            catch (Exception)
+            {
+                return null!;
+            }
+        }
 
-        //public Task<string> GetItem(long bookId)
-        //{
-        //    throw new NotImplementedException();
-        //}
 
-        //public Task<string> GetItemPrice(long bookId)
-        //{
-        //    throw new NotImplementedException();
+        public async Task<string> EnlistPurchase(long? bookId, uint? count)
+        {
+            if (bookId is null || count is null)
+            {
+                return null!;
+            }
 
-        //}
+            ITransactionCoordinator? transactionProxy = ServiceProxy.Create<ITransactionCoordinator>(new Uri(transactionCoordinatorPath));
 
-        //public Task<List<string>> ListAvailableItems()
-        //{
-        //    throw new NotImplementedException();
-        //}
+            try
+            {
+                return await transactionProxy.EnlistPurchase(bookId!.Value, count!.Value);
+            }
+            catch (Exception)
+            {
+                return null!;
+            }
+        }
 
-        //public Task<List<string>> ListClients()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public async Task<string> GetBook(long? bookId)
+        {
+            if (bookId is null)
+            {
+                return null!;
+            }
+
+            ITransactionCoordinator? transactionProxy = ServiceProxy.Create<ITransactionCoordinator>(new Uri(transactionCoordinatorPath));
+
+            try
+            {
+                return await transactionProxy.GetBook(bookId!.Value);
+            }
+            catch (Exception)
+            {
+                return null!;
+            }
+        }
+
+        public async Task<string> GetBookPrice(long? bookId)
+        {
+            if (bookId is null)
+            {
+                return null!;
+            }
+
+            ITransactionCoordinator? transactionProxy = ServiceProxy.Create<ITransactionCoordinator>(new Uri(transactionCoordinatorPath));
+
+            try
+            {
+                return await transactionProxy.GetBookPrice(bookId!.Value);
+            }
+            catch (Exception)
+            {
+                return null!;
+            }
+        }
+
+    
+        public async Task<List<string>> ListBanksClients()
+        {
+            ITransactionCoordinator? transactionProxy = ServiceProxy.Create<ITransactionCoordinator>(new Uri(transactionCoordinatorPath));
+
+            try
+            {
+                return await transactionProxy.ListBanksClients();
+            }
+            catch (Exception)
+            {
+                return null!;
+            }
+        }
+
+        public async Task<string> EnlistMoneyTransfer(long? userSend, long? userReceive, double? amount)
+        {
+            if (userSend is null || userReceive is null || amount is null)
+            {
+                return null!;
+            }
+
+            ITransactionCoordinator? transactionProxy = ServiceProxy.Create<ITransactionCoordinator>(new Uri(transactionCoordinatorPath));
+
+            try
+            {
+                return await transactionProxy.EnlistMoneyTransfer(userSend!.Value, userReceive!.Value, amount!.Value);
+            }
+            catch (Exception)
+            {
+                return null!;
+            }
+        }
 
         protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
         {
